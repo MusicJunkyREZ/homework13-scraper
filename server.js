9b1eb4b6b42184ff1ec118e08319faf11cb39831
MusicJunkyREZ <reidzuckerman@gmail.com>
2019-04-24 20:46:12 -0600

    deploying to heroku

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/homework13";
 
 mongoose.connect(MONGODB_URI);
 
